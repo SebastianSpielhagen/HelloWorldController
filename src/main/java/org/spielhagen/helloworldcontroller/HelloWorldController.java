cdacff59a2d6c9c0d32ace5648c0749ac79e5556
SebastianSpielhagen <ssp.java@icloud.com>
2023-11-22 11:22:02 +0100

    add step 3

--- src/main/java/org/spielhagen/helloworldcontroller/HelloWorldController.java
+++ src/main/java/org/spielhagen/helloworldcontroller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package org.spielhagen.helloworldcontroller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
     @RestController
@@ -13,5 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
         @GetMapping("/api/hello")
         public String getHelloMessage() {
             return "Hello, World!";
+        }
+        @GetMapping("/api/hello/{name}")
+        public String getCustomizedHello(@PathVariable String name) {
+            return "Hello, " + name + "!";
         }
 }

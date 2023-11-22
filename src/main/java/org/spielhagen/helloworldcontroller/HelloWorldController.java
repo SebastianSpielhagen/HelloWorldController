4c6203fe5731acc3ad827bc66dd7f90f88ca8738
SebastianSpielhagen <ssp.java@icloud.com>
2023-11-22 11:28:37 +0100

    add message controller

--- src/main/java/org/spielhagen/helloworldcontroller/HelloWorldController.java
+++ src/main/java/org/spielhagen/helloworldcontroller/HelloWorldController.java
@@ -1,12 +1,24 @@
 package org.spielhagen.helloworldcontroller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
 
-    @RestController
+
+@RestController
     public class HelloWorldController {
 
+        private List<Message> messages = new ArrayList<>();
+
+        @GetMapping("/api/messages")
+        public List<Message> getAllMessages() {
+            return messages;
+        }
+
+        @PostMapping("/api/messages")
+        public void addMessage(@RequestBody Message message) {
+            messages.add(message);
+        }
         @GetMapping
         public String sayHello(){
             return "Hallo Welt2";
